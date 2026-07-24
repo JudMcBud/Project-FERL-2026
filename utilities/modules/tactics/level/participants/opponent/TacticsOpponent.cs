c20601783495b7acea7d6709cc3e6adbfc5271f8
Judson Hester <judsonhester01@gmail.com>
2026-07-24 18:55:14 -0500

    Finished TacticsLevel.cs
    Finished creating and writing TacticsLevel.cs, which is the topmost level script.

---
+++ utilities/modules/tactics/level/participants/opponent/TacticsOpponent.cs
@@ -0,0 +1,12 @@
+using System;
+using Game.Modules.Tactics.Level.Participants.TacticsParticipant;
+using Godot;
+
+public partial class TacticsOpponent : TacticsParticipant
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+}
