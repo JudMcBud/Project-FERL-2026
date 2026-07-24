cc414db11f6c265611b590df46cac5568c66c930
Judson Hester <judsonhester01@gmail.com>
2026-07-24 13:54:29 -0500

    Basic Scene Loading & Main Implemented

---
+++ utilities/Main.cs
@@ -0,0 +1,42 @@
+using Game.Stages.TacticsLevel;
+using Godot;
+
+public partial class Main : Node3D
+{
+    #region: --- Props ---
+    private TacticsLevel levelInstance;
+    private Node3D world;
+
+    private Button demoMapButton;
+    #endregion
+
+    #region --- Processing ---
+    public override void _Ready()
+    {
+        world = GetNode<Node3D>("World");
+        demoMapButton = GetNode<Button>("UI/MapSelector/LoadMap0");
+        demoMapButton.Pressed += OnDemoMapButtonPressed;
+        demoMapButton.GrabFocus();
+    }
+    #endregion
+
+    #region --- Signals ---
+    private void OnDemoMapButtonPressed()
+    {
+        LoadLevel("TestLevel");
+    }
+    #endregion
+
+    #region --- Methods ---
+    private void LoadLevel(string levelName)
+    {
+        UnloadLevel();
+        string levelPath = $"res://stages/tactics/test/{levelName}.tscn";
+        var levelInstance = GD.Load<PackedScene>(levelPath).Instantiate<TacticsLevel>();
+        world.AddChild(levelInstance);
+        GetNode<CenterContainer>("UI/MapSelector").Visible = false;
+    }
+
+    private void UnloadLevel() { }
+    #endregion
+}
