c89a41825a7f449cc429d20aac9244cb0518f40a
Judson Hester <judsonhester01@gmail.com>
2026-07-25 12:08:54 -0500

    Finished TacticsCameraResource.cs

--- utilities/models/view/camera/tactics/TacticsCameraResource.cs
+++ utilities/models/view/camera/tactics/TacticsCameraResource.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace Game.Models.View.Camera.Tactics.TacticsCameraResource;
@@ -5,7 +6,49 @@ namespace Game.Models.View.Camera.Tactics.TacticsCameraResource;
 [GlobalClass]
 public partial class TacticsCameraResource : Resource
 {
+    [Signal]
+    public delegate void CalledMoveCameraEventHandler();
+
+    [Signal]
+    public delegate void CalledFreeLookEventHandler();
+
+    [Signal]
+    public delegate void CalledRotateCameraEventHandler();
+
+    #region Movement
+    [ExportCategory("Movement")]
+    [Export(PropertyHint.Range, "1,100,")]
+    public int moveSpeed { get; set; } = 10;
+    private static float rotSpeed;
+
+    [Export(PropertyHint.Range, "1,100,")]
+    public float RotationSpeed
+    {
+        get { return rotSpeed; }
+        set { rotSpeed = (float)value / 10.0f; }
+    }
+
+    [Export(PropertyHint.Range, "0.01, 1,")]
+    public float smoothing = 0.1f;
+
+    private Vector3 targetVelocity = Vector3.Zero;
+
+    private Node3D target = null;
+    #endregion
+
+    #region Zoom
+    [ExportCategory("Zoom")]
+    private static float zoomSpeed;
+
+    [Export(PropertyHint.Range, "0.01, 1,")]
+    public float CameraZoomSpeed
+    {
+        get => field;
+        set => field = (float)value;
+    } = 0.5f;
+    #endregion
     #region Panning
+    [ExportCategory("Panning")]
     [Export]
     public float boundaryRadius = 10.0f;
     #endregion
