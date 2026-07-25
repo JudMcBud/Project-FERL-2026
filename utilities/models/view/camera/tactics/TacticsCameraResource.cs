181e0151b310692364ca683e7076d9f79c695962
Judson Hester <judsonhester01@gmail.com>
2026-07-25 12:17:20 -0500

    Finished TacticsCameraResource.cs
    I don't know what the heck happened but almost the entire file disappeared. Fortunately Ctrl + Z saved me. Committing this now. Very scary. But (hopefully) saved now.

--- utilities/models/view/camera/tactics/TacticsCameraResource.cs
+++ utilities/models/view/camera/tactics/TacticsCameraResource.cs
@@ -7,13 +7,18 @@ namespace Game.Models.View.Camera.Tactics.TacticsCameraResource;
 public partial class TacticsCameraResource : Resource
 {
     [Signal]
-    public delegate void CalledMoveCameraEventHandler();
+    public delegate void MoveCameraEventHandler(
+        float horizontal,
+        float vertical,
+        bool joystick,
+        float delta
+    );
 
     [Signal]
-    public delegate void CalledFreeLookEventHandler();
+    public delegate void FreeLookEventHandler(float delta, float twist);
 
     [Signal]
-    public delegate void CalledRotateCameraEventHandler();
+    public delegate void RotateCameraEventHandler();
 
     #region Movement
     [ExportCategory("Movement")]
@@ -24,8 +29,8 @@ public partial class TacticsCameraResource : Resource
     [Export(PropertyHint.Range, "1,100,")]
     public float RotationSpeed
     {
-        get { return rotSpeed; }
-        set { rotSpeed = (float)value / 10.0f; }
+        get => field;
+        set => field = (float)value / 10.0f;
     }
 
     [Export(PropertyHint.Range, "0.01, 1,")]
@@ -38,18 +43,105 @@ public partial class TacticsCameraResource : Resource
 
     #region Zoom
     [ExportCategory("Zoom")]
-    private static float zoomSpeed;
+    private static float zoomSpeed = 0.5f;
 
     [Export(PropertyHint.Range, "0.01, 1,")]
     public float CameraZoomSpeed
     {
-        get => field;
-        set => field = (float)value;
-    } = 0.5f;
+        get => zoomSpeed;
+        set => zoomSpeed = (float)value;
+    }
+
+    [Export(PropertyHint.Range, "0.01, 1,")]
+    public float zoomSmoothness = 0.1f;
+
+    [Export(PropertyHint.Range, "0.01, 1,")]
+    public float zoomDuration = 0.5f;
+
+    [Export(PropertyHint.Range, "0.1, 50,")]
+    public float minZoom = 1.0f;
+
+    [Export(PropertyHint.Range, "10.0, 100,")]
+    public float maxZoom = 10.0f;
+
+    public float currentFOV = 50.0f;
+    public float targetFOV = 50.0f;
     #endregion
+
     #region Panning
     [ExportCategory("Panning")]
     [Export]
     public float boundaryRadius = 10.0f;
+    public Vector3 boundaryCenter = Vector3.Zero;
+
+    [Export(PropertyHint.Range, "1, 50,")]
+    public float borderPanPixelThreshold = 1.0f;
+
+    [Export(PropertyHint.Range, "0.01, 1,")]
+    public float mousePanSpeed = 0.5f;
+
+    [Export(PropertyHint.Range, "0.01, 1,")]
+    public float joyPanSpeed = 0.5f;
+
+    private const float PanningDelay = 0.05f;
+    private const float panningTimer = 0.0f;
+    #endregion
+
+    #region Rotation
+    [ExportCategory("Rotation")]
+    //Probably want to turn quadrant snapping off later
+    [Export(PropertyHint.Range, "0.1, 10,")]
+    public float quadSnapDuration = 0.2f;
+
+    private bool isSnappingToQuad = false;
+    private bool isRotating = false;
+    private static int xRotation;
+
+    [Export]
+    public int VerticalRotation
+    {
+        get => xRotation;
+        set => xRotation = value;
+    }
+
+    private static int yRotation;
+
+    [Export]
+    public int HorizontalRotation
+    {
+        get => yRotation;
+        set => yRotation = value;
+    }
+
+    [Export]
+    public int zRotation;
+
+    private Vector2 mousePosition;
+
+    private static bool inFreeLook;
+
+    // Probably want to change this to a recenter camera button press input instead of a timer
+    private float freeLookTimer = 0.0f;
+    private const float FreeLookTimeout = 0.05f;
+    private static float twistInput;
+    private static float pitchInput;
+    private Vector2I viewportSize;
+    #endregion
+
+    #region Signals
+    private void MoveCamera(float horizontal, float vertical, bool joystick, float delta)
+    {
+        EmitSignal(SignalName.MoveCamera, horizontal, vertical, joystick, delta);
+    }
+
+    private void RotateCamera(float delta, float twist = 0.0f)
+    {
+        EmitSignal(SignalName.RotateCamera, delta, twist);
+    }
+
+    private void FreeLook(float delta)
+    {
+        EmitSignal(SignalName.FreeLook, delta);
+    }
     #endregion
 }
