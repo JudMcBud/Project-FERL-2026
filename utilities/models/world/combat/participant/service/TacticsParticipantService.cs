d36a2a5ce7c7c7fb0086aa7c0e6bd08ab371a23a
Judson Hester <judsonhester01@gmail.com>
2026-07-25 14:30:09 -0500

    TacticsParticipantService.cs Finished
    Also created TacticsParticipantCombatService.cs and TacticsParticipantTurnServices.cs. They are empty atm.

--- utilities/models/world/combat/participant/service/TacticsParticipantService.cs
+++ utilities/models/world/combat/participant/service/TacticsParticipantService.cs
@@ -1,6 +1,69 @@
-using Godot;
 using System;
+using Game.Models.View.Camera.Tactics.TacticsCameraResource;
+using Game.Models.View.Control.Tactics.TacticsControlsResource;
+using Game.Modules.Tactics.Level.Participants.TacticsParticipant;
+using Godot;
 
 public partial class TacticsParticipantService : RefCounted
 {
+    private TacticsParticipantResource resource;
+    private TacticsCameraResource camera;
+    private TacticsControlsResource controls;
+    private TacticsParticipantTurnService turnService;
+    private TacticsParticipantCombatService combatService;
+
+    public void Init(
+        TacticsParticipantResource _resource,
+        TacticsCameraResource _camera,
+        TacticsControlsResource _controls
+    )
+    {
+        resource = _resource;
+        camera = _camera;
+        controls = _controls;
+        // turnService = TacticsParticipantTurnService.new(resource, camera, controls);
+        // combatService = TacticsParticipantCombatService.new(resource, camera, controls);
+    }
+
+    public void Setup(TacticsParticipant _participant)
+    {
+        if (controls == null)
+            GD.PushError(
+                "TacticsControls needs a ControlResource from /utilities/models/view/control/tactics/"
+            );
+        if (camera == null)
+            GD.PushError(
+                "TacticsControls needs a CameraResource from /utilities/models/view/camera/tactics/"
+            );
+        if (resource == null)
+            GD.PushError(
+                "TacticsControls needs a ParticipantResource from /utilities/models/world/combat/participant/"
+            );
+    }
+
+    public void Act(float delta, bool isPlayer, Node3D parent, TacticsParticipant participant)
+    {
+        if (isPlayer)
+        {
+            TacticsPlayer player = parent as TacticsPlayer;
+            // turnService.HandlePlayerTurn(delta, player, participant);
+        }
+        else
+        {
+            TacticsOpponent opponent = parent as TacticsOpponent;
+            // turnService.HandleOpponentTurn(delta, opponent, participant);
+        }
+    }
+
+    public void Configure(TacticsCameraResource myCamera, TacticsControlsResource myControls)
+    {
+        camera = myCamera;
+        controls = myControls;
+    }
+
+    public bool IsConfigured(Node3D parent)
+    {
+        // return parent.IsPawnConfigured();
+        return true;
+    }
 }
