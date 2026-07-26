72d9d8a2c02950cf6ed2b328a0fe47f22432d284
Judson Hester <judsonhester01@gmail.com>
2026-07-26 15:18:15 -0500

    TacticsArena.cs Finished

--- utilities/modules/tactics/level/arena/TacticsArena.cs
+++ utilities/modules/tactics/level/arena/TacticsArena.cs
@@ -1,13 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using Game.Modules.Tactics.Level.Pawn.TacticsPawn;
 using Godot;
 
 public partial class TacticsArena : Node3D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    [Export]
+    public TacticsArenaResource resource;
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+    public TacticsArenaService service;
 
-    public void ConfigureTiles() { }
+    public override void _Ready()
+    {
+        resource = GD.Load<TacticsArenaResource>(
+            "res://utilities/models/world/combat/arena/tacticsArenaResource.tres"
+        );
+        service = new TacticsArenaService(resource);
+        service.Setup(this);
+    }
+
+    public void ResetAllTileMarkers()
+    {
+        service.ResetAllTileMarkers(this);
+    }
+
+    public void ConfigureTiles()
+    {
+        service.ConfigureTiles(this);
+    }
+
+    public void ProcessSurroundingTiles(
+        TacticsTile rootTile,
+        float height,
+        List<Node3D> alliesOnMap = null
+    )
+    {
+        alliesOnMap ??= [];
+        service.ProcessSurroundingTiles(rootTile, height, alliesOnMap);
+    }
+
+    public List<TacticsTile> GetPathfindingTilestack(TacticsTile to)
+    {
+        return service.GetPathfindingTileStack(to);
+    }
+
+    public TacticsTile GetNearestTargetAdjacentTile(TacticsPawn pawn, List<TacticsPawn> targetPawns)
+    {
+        return service.GetNearestTargetAdjacentTile(pawn, targetPawns);
+    }
+
+    public TacticsPawn GetWeakestAttackablePawn(List<TacticsPawn> pawnList)
+    {
+        return service.GetWeakestAttackablePawn(pawnList);
+    }
+
+    public void MarkHoveTile(TacticsTile tile)
+    {
+        service.MarkHoveTile(tile);
+    }
+
+    public void MarkReachableTiles(TacticsTile root)
+    {
+        service.MarkReachableTiles(root);
+    }
+
+    public void MarkAttackableTiles(TacticsTile root, float distance)
+    {
+        service.MarkAttackableTiles(this, root, distance);
+    }
 }
