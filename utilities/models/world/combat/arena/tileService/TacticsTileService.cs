f6d47a84ab590b6d2fd15ee0fea0ac3dc8c59064
Judson Hester <judsonhester01@gmail.com>
2026-07-28 15:32:23 -0500

    TacticsTileService.cs Finished
    Because Godot is dynamically typed, the pre-translation script was able to call functions on _staticBody that don't exist on it before it changes type. The "dynamic" keyword was used so it may introduce fatal errors. Checks were put in place to stop crashes, but it may not suffice. If on completion, I am encountering bugs, I should look here first.

--- utilities/models/world/combat/arena/tileService/TacticsTileService.cs
+++ utilities/models/world/combat/arena/tileService/TacticsTileService.cs
@@ -1,13 +1,36 @@
-using System;
+using System.Runtime.Serialization.Formatters;
 using Godot;
 
 public partial class TacticsTileService : Node3D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public const string TileSource =
+        "res://utilities/modules/tactics/level/arena/tile/TacticsTile.cs";
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+    public void TilesIntoStaticbodies(Node3D tiles)
+    {
+        foreach (MeshInstance3D _t in tiles.GetChildren())
+        {
+            _t.CreateTrimeshCollision();
+            dynamic _staticBody = _t.GetChild<StaticBody3D>(0);
+            _staticBody.Position = _t.Position;
 
-    public void TilesIntoStaticbodies(Node3D tiles) { }
+            _t.Position = Vector3.Zero;
+            _t.Name = "Tile";
+            _t.RemoveChild(_staticBody);
+            tiles.RemoveChild(_t);
+            _staticBody.AddChild(_t);
+            _staticBody.SetScript(GD.Load<Script>(TileSource));
+
+            if (_staticBody is TacticsTile)
+                _staticBody.ConfigureTile();
+            else
+                GD.PushError(
+                    "_staticBody is not a TacticsTile. Script: TacticsTileService.cs, Function: TilesIntoStaticBodies, Line: 23"
+                );
+
+            _staticBody.SetProcess(true);
+
+            tiles.AddChild(_staticBody);
+        }
+    }
 }
