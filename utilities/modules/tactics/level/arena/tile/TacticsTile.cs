db14ee4e1e822fa4daac413b9018a90ccb789d7f
Judson Hester <judsonhester01@gmail.com>
2026-07-28 05:46:01 -0500

    TacticsTile.cs Finished

--- utilities/modules/tactics/level/arena/tile/TacticsTile.cs
+++ utilities/modules/tactics/level/arena/tile/TacticsTile.cs
@@ -1,36 +1,89 @@
 using System;
 using System.Collections.Generic;
+using Game.Models.Config.TacticsConfig;
 using Godot;
 
 public partial class TacticsTile : StaticBody3D
 {
+    #region --- Props ---
+    public PackedScene tileRaycast = GD.Load<PackedScene>(
+        "res://utilities/modules/tactics/level/arena/tile/raycast/tileRaycast.tscn"
+    );
+
     public bool reachable = false;
     public bool attackable = false;
     public bool hover = false;
+
     public TacticsTile pfRoot;
     public float pfDistance;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public StandardMaterial3D hoverMat = TacticsConfig.matColor["hover"];
+    public StandardMaterial3D reachableMat = TacticsConfig.matColor["reachable"];
+    public StandardMaterial3D hoverReachableMat = TacticsConfig.matColor["hoverReachable"];
+    public StandardMaterial3D attackableMat = TacticsConfig.matColor["attackable"];
+    public StandardMaterial3D hoverAttackableMat = TacticsConfig.matColor["hoverAttackable"];
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+    #endregion
+    #region --- Processing ---
+    public override void _Process(double delta)
+    {
+        MeshInstance3D tile = GetNodeOrNull("Tile") as MeshInstance3D;
+        if (tile == null)
+            return;
 
-    public void ResetMarker() { }
+        tile.Visible = attackable || reachable || hover;
+
+        if (hover)
+        {
+            if (reachable)
+                tile.MaterialOverride = hoverReachableMat;
+            else if (attackable)
+                tile.MaterialOverride = hoverAttackableMat;
+            else
+                tile.MaterialOverride = hoverMat;
+        }
+        else
+        {
+            if (reachable)
+                tile.MaterialOverride = reachableMat;
+            else if (attackable)
+                tile.MaterialOverride = attackableMat;
+        }
+    }
+    #endregion
+
+    #region --- Methods ---
 
     public List<TacticsTile> GetNeighbors(float height)
     {
-        List<TacticsTile> r = [];
-        return r;
-    }
-
-    public bool IsTaken()
-    {
-        return false;
+        return GetNode<TacticsTileRaycast>("RayCasting").GetAllNeighbors(height);
     }
 
     public Object GetTileOccupier()
     {
-        return new Object();
+        return GetNode<TacticsTileRaycast>("RayCasting").GetObjectAbove();
     }
+
+    public bool IsTaken()
+    {
+        return GetTileOccupier() != null;
+    }
+
+    public void ResetMarkers()
+    {
+        pfRoot = null;
+        pfDistance = 0;
+        reachable = false;
+        attackable = false;
+    }
+
+    public void ConfigureTile()
+    {
+        hover = false;
+        Node instance = tileRaycast.Instantiate();
+        AddChild(instance);
+        ResetMarkers();
+    }
+
+    #endregion
 }
