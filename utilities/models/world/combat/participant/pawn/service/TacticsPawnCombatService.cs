bf364e16245e5e7fd8614670e9a63cdcf186b6ca
Judson Hester <judsonhester01@gmail.com>
2026-08-05 05:39:05 -0500

    TacticsPawnCombatService.cs Finished

--- utilities/models/world/combat/participant/pawn/service/TacticsPawnCombatService.cs
+++ utilities/models/world/combat/participant/pawn/service/TacticsPawnCombatService.cs
@@ -1,4 +1,32 @@
 using System;
+using Game.Modules.Tactics.Level.Pawn.TacticsPawn;
 using Godot;
 
-public partial class TacticsPawnCombatService : RefCounted { }
+public partial class TacticsPawnCombatService : RefCounted
+{
+    public bool AttackTargetPawn(TacticsPawn pawn, TacticsPawn targetPawn, double delta)
+    {
+        pawn.service.movement.LookAtDirection(
+            pawn,
+            targetPawn.GlobalPosition - pawn.GlobalPosition
+        );
+
+        if (
+            pawn.resource.canAttack
+            && pawn.resource.waitDelay > TacticsPawnResource.MinTimeForAttack / 4.0f
+        )
+        {
+            targetPawn.stats.ApplyToCurrentHealth(-pawn.stats.attackPower);
+            pawn.resource.SetAttacking(false);
+        }
+
+        if (pawn.resource.waitDelay < TacticsPawnResource.MinTimeForAttack)
+        {
+            pawn.resource.waitDelay += (float)delta;
+            return false;
+        }
+
+        pawn.resource.waitDelay = 0.0f;
+        return true;
+    }
+}
