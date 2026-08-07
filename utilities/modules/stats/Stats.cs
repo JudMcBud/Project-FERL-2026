8db851849af2b746a2fea4f8e7913dfe9d77e7ce
Judson Hester <judsonhester01@gmail.com>
2026-08-07 08:36:59 -0500

    Stats.cs Finished
    Changed type of jump in StatsResource.cs so there doesn't need to be any flooring or type casting.

--- utilities/modules/stats/Stats.cs
+++ utilities/modules/stats/Stats.cs
@@ -1,16 +1,39 @@
 using System;
 using Godot;
+using Godot.Collections;
 
 public partial class Stats : Node
 {
-    #region Base Stats
+    public Dictionary modifiers = new Dictionary();
     public string overrideName;
+    public string expertise;
+    public int level = 1;
+
+    #region Base Stats
+    public int movement;
     public int jump;
     public int maxHealth;
     public int currentHealth;
     public string sprite;
-    public int attackPower;
     #endregion
+    #region Offensive Stats
+    public int attackPower;
+    public int attackRange;
+    #endregion
+
+    public void ImportStats(StatsResource stats)
+    {
+        overrideName = stats.overrideName;
+        expertise = stats.expertise;
+        level = stats.level;
+        movement = stats.movement;
+        jump = stats.jump;
+        maxHealth = stats.maxHealth;
+        currentHealth = maxHealth;
+        sprite = stats.sprite;
+        attackPower = stats.attackPower;
+        attackRange = stats.attackRange;
+    }
 
     public void ApplyToCurrentHealth(int changeinHealth)
     {
