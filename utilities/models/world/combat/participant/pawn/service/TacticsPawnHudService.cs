31937809c5489907aa17efc503ab901f757061db
Judson Hester <judsonhester01@gmail.com>
2026-08-07 08:04:38 -0500

    TacticsPawnHudServices.cs Finished

--- utilities/models/world/combat/participant/pawn/service/TacticsPawnHudService.cs
+++ utilities/models/world/combat/participant/pawn/service/TacticsPawnHudService.cs
@@ -1,4 +1,21 @@
 using System;
+using Game.Modules.Tactics.Level.Pawn.TacticsPawn;
 using Godot;
 
-public partial class TacticsPawnHudService : RefCounted { }
+public partial class TacticsPawnHudService : RefCounted
+{
+    public void UpdateCharacterHealth(TacticsPawn pawn)
+    {
+        Label3D _healthLabel = pawn.GetNode<Label3D>("Character/CharacterUI/HealthLabel");
+        _healthLabel.Text =
+            pawn.stats.currentHealth.ToString() + "/" + pawn.stats.maxHealth.ToString();
+    }
+
+    public void TintWhenUnableToAct(TacticsPawn pawn)
+    {
+        TacticsPawnSprite _characterNode = pawn.GetNode<TacticsPawnSprite>("Character");
+        _characterNode.Modulate = !pawn.CanAct()
+            ? new Color(0.5f, 0.5f, 0.5f)
+            : new Color(1f, 1f, 1f);
+    }
+}
