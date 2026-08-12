09ad2743191ccba05431f81d7e01105e0c4b556c
Judson Hester <judsonhester01@gmail.com>
2026-08-12 18:32:37 -0500

    TacticsCamera.cs Finished

--- utilities/modules/tactics/camera/TacticsCamera.cs
+++ utilities/modules/tactics/camera/TacticsCamera.cs
@@ -1,49 +1,69 @@
 using System;
+using Game.Models.View.Camera.Tactics.TacticsCameraResource;
+using Game.Models.View.Control.Tactics.TacticsControlsResource;
 using Godot;
 
 public partial class TacticsCamera : CharacterBody3D
 {
+    [Export]
+    public TacticsCameraResource resource = GD.Load<TacticsCameraResource>(
+        "res://utilities/models/view/camera/tactics/camera.tres"
+    );
+
+    [Export]
+    public TacticsControlsResource controls = GD.Load<TacticsControlsResource>(
+        "res://utilities/models/view/control/tactics/control.tres"
+    );
+
+    public static TacticsCameraService service;
     public Node3D tPivot;
     public Node3D pPivot;
 
     public Camera3D camNode;
-    public const float Speed = 5.0f;
-    public const float JumpVelocity = 4.5f;
 
-    public override void _PhysicsProcess(double delta)
+    public override void _Ready()
     {
-        Vector3 velocity = Velocity;
+        tPivot = GetNode<Node3D>("TwistPivot");
+        pPivot = GetNode<Node3D>("TwistPivot/PitchPivot");
+        camNode = GetNode<Camera3D>("TwistPivot/PitchPivot/Camera3D");
 
-        // Add the gravity.
-        if (!IsOnFloor())
-        {
-            velocity += GetGravity() * (float)delta;
-        }
+        service = new TacticsCameraService(resource, controls);
+        service.Setup(this, camNode);
+        resource.boundaryCenter = GlobalPosition;
 
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-        {
-            velocity.Y = JumpVelocity;
-        }
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        if (direction != Vector3.Zero)
-        {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-        }
-
-        Velocity = velocity;
-        MoveAndSlide();
+        resource.RotateCamera += RotateCamera;
+        resource.MoveCamera += MoveCamera;
     }
 
-    public void MoveCamera(float h, float v, bool joystick, double delta) { }
+    public override void _Process(double delta)
+    {
+        service.Process(delta, this);
+    }
+
+    public void MoveCamera(float h, float v, bool joystick, double delta)
+    {
+        service.move.MoveCamera(h, v, joystick, delta, this);
+    }
+
+    public void RotateCamera(double delta, int twist = 0)
+    {
+        resource.isRotating = true;
+        service.rotate.AddAngleToHorizontalRotation(twist);
+        service.rotate.RotateCamera(delta, tPivot, pPivot);
+    }
+
+    public void FreeLook(double delta)
+    {
+        service.rotate.FreeLook(delta, tPivot, pPivot);
+    }
+
+    public static void ZoomCamera(float zoomIncrement)
+    {
+        service.zoom.ZoomCamera(zoomIncrement);
+    }
+
+    public void ResetCamZoom()
+    {
+        service.zoom.ResetCamZoom(camNode, this);
+    }
 }
