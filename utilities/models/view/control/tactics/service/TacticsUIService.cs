ff3081255b4f7d03b45d321e9533a475372a9997
Judson Hester <judsonhester01@gmail.com>
2026-08-13 06:01:35 -0500

    TacticsUIService.cs

--- utilities/models/view/control/tactics/service/TacticsUIService.cs
+++ utilities/models/view/control/tactics/service/TacticsUIService.cs
@@ -1,6 +1,42 @@
-using Godot;
 using System;
+using Game.Models.View.Control.Tactics.TacticsControlsResource;
+using Game.Modules.Tactics.Level.Pawn.TacticsPawn;
+using Godot;
 
 public partial class TacticsUIService : RefCounted
 {
+    public TacticsControlsResource controls;
+
+    public TacticsUIService(TacticsControlsResource _controls)
+    {
+        controls = _controls;
+    }
+
+    public void UpdateControllerHints(TacticsControls ctrl)
+    {
+        if (controls.isJoystick)
+        {
+            ctrl.GetNode<TextureRect>("ControllerHints").Texture = ctrl.layoutXbox;
+        }
+        else
+        {
+            ctrl.GetNode<TextureRect>("ControllerHints").Texture = ctrl.layoutPC;
+        }
+    }
+
+    public void SetActionsMenuVisibility(bool v, TacticsPawn p, TacticsControls ctrl)
+    {
+        if (!ctrl.GetNode<BoxContainer>("HBoxContainer/Actions").Visible)
+        {
+            ctrl.GetNode<Button>("HBox/Actions/Move").GrabFocus();
+        }
+
+        ctrl.GetNode<BoxContainer>("HBox/Actions").Visible = v && p.CanAct();
+
+        if (p == null)
+            return;
+
+        ctrl.GetNode<Button>("HBox/Actions/Move").Disabled = !p.resource.canMove;
+        ctrl.GetNode<Button>("HBox/Actions/Attack").Disabled = !p.resource.canAttack;
+    }
 }
