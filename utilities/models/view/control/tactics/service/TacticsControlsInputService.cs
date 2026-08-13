a24621e08ffbd475f29f9d4b068f450a90566002
Judson Hester <judsonhester01@gmail.com>
2026-08-12 19:29:14 -0500

    TacticsControlsInputService.cs Finished

---
+++ utilities/models/view/control/tactics/service/TacticsControlsInputService.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using Game.Models.Config.TacticsConfig;
+using Game.Models.View.Control.Tactics.TacticsControlsResource;
+using Godot;
+
+public partial class TacticsControlsInputService : RefCounted
+{
+    public TacticsControlsResource controls;
+    public MouseClickCapture inputCapture;
+
+    public TacticsControlsInputService(TacticsControlsResource _controls, Node _inputCapture)
+    {
+        controls = _controls;
+        inputCapture = (MouseClickCapture)_inputCapture;
+    }
+
+    public void UpdateMouseMode()
+    {
+        // May need to adjust mouse modes
+        Input.MouseModeEnum mouseMode = controls.isJoystick
+            ? Input.MouseModeEnum.Hidden
+            : Input.MouseModeEnum.Visible;
+        Input.MouseMode = mouseMode;
+    }
+
+    public void HandleInput(InputEvent e)
+    {
+        controls.isJoystick = e is InputEventJoypadButton || e is InputEventJoypadMotion;
+    }
+
+    public Object Get3DCanvasMousePosition(int collisionMask, TacticsControls ctrl)
+    {
+        if (IsMouseHoveringUIElem(ctrl))
+            return null;
+        if (inputCapture != null)
+            return inputCapture.ProjectMousePosition(collisionMask, controls.isJoystick);
+        else
+            return null;
+    }
+
+    public bool IsMouseHoveringUIElem(TacticsControls ctrl, List<string> elm = null)
+    {
+        if (elm == null)
+            elm = TacticsConfig.uiElem;
+
+        foreach (string e in elm)
+        {
+            if (ctrl.GetNode<Control>(e).Visible)
+            {
+                switch (e)
+                {
+                    case "%Actions":
+                        foreach (Button action in ctrl.GetNode<Control>(e).GetChildren())
+                        {
+                            if (
+                                action
+                                    .GetGlobalRect()
+                                    .HasPoint(ctrl.GetViewport().GetMousePosition())
+                            )
+                                return true;
+                        }
+                        break;
+                    case "%Hints":
+                        foreach (TextureRect hint in ctrl.GetNode<Control>(e).GetChildren())
+                        {
+                            if (
+                                hint.GetGlobalRect().HasPoint(ctrl.GetViewport().GetMousePosition())
+                            )
+                                return true;
+                        }
+                        break;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ utilities/modules/tactics/controls/TacticsControls.cs
@@ -0,0 +1,33 @@
+using System;
+using Game.Models.View.Camera.Tactics.TacticsCameraResource;
+using Game.Models.View.Control.Tactics.TacticsControlsResource;
+using Game.Modules.Tactics.Level.Pawn.TacticsPawn;
+using Godot;
+
+public partial class TacticsControls : Control
+{
+    #region Props
+    [Export]
+    public TacticsControlsResource controls = GD.Load<TacticsControlsResource>(
+        "res://utilities/models/view/control/tactics/control.tres"
+    );
+
+    [Export]
+    public TacticsCameraResource tacticsCam = GD.Load<TacticsCameraResource>(
+        "res://utilities/models/view/camera/tactics/camera.tres"
+    );
+
+    [Export]
+    public TacticsParticipantResource participant = GD.Load<TacticsParticipantResource>(
+        "res://utilities/models/world/combat/participant/participant.tres"
+    );
+
+    [Export]
+    public TacticsArenaResource arena = GD.Load<TacticsArenaResource>(
+        "res://utilities/models/world/combat/arena/tacticsArenaResource.tres"
+    );
+
+    public TacticsPawn currentPawn = null;
+    public TacticsControlsService service;
+    #endregion
+}
