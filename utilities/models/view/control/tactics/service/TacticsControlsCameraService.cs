7a263b5598ada1faf69347f41b0aa4a7533e3b96
Judson Hester <judsonhester01@gmail.com>
2026-08-13 19:25:37 -0500

    TacticsControlsCameraService.cs Finished

--- utilities/models/view/control/tactics/service/TacticsControlsCameraService.cs
+++ utilities/models/view/control/tactics/service/TacticsControlsCameraService.cs
@@ -1,6 +1,22 @@
-using Godot;
 using System;
+using Game.Models.View.Camera.Tactics.TacticsCameraResource;
+using Godot;
 
 public partial class TacticsControlsCameraService : RefCounted
 {
+    TacticsCameraResource tCam;
+
+    public TacticsControlsCameraService(TacticsCameraResource _tCam)
+    {
+        tCam = _tCam;
+    }
+
+    public void MoveCamera(double delta, bool isJoystick)
+    {
+        float h = -Input.GetActionStrength("cameraLeft") + Input.GetActionStrength("cameraRight");
+        float v =
+            Input.GetActionStrength("cameraForward") - Input.GetActionStrength("cameraBackwards");
+
+        tCam.MoveCameraHandler(h, v, isJoystick, delta);
+    }
 }
